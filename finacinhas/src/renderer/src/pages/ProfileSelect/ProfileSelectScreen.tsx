e36c0713ac8a92eb16adee472c18948431a0e435
Gabriel Al-Samir <alsamir.gabriel@gmail.com>
2024-12-12 16:15:30 -0300

    wip: merge and approve

--- finacinhas/src/renderer/src/pages/ProfileSelect/ProfileSelectScreen.tsx
+++ finacinhas/src/renderer/src/pages/ProfileSelect/ProfileSelectScreen.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import logo from '../assets/Logo-Subtitle.svg';
-import '../styles/ProfileSelectScreen.css';
-
+import React from 'react'
+import logo from '../../assets/Logo-Subtitle.svg'
+import '../ProfileSelect/ProfileSelectScreen.styles.css'
 
 const ProfileSelectScreen: React.FC = () => {
-
   return (
     <div className="containerProfileSelectScreen">
       <header className="headerProfileSelectScreen">
-        <img src={logo} className="logoProfileSelectScreen" alt="logo"/>
+        <img src={logo} className="logoProfileSelectScreen" alt="logo" />
       </header>
       <main className="mainProfileSelectScreen">
         <h2>Você é um...</h2>
@@ -16,7 +14,7 @@ const ProfileSelectScreen: React.FC = () => {
         <button className="ProfileButton">SOU PROFESSOR</button>
       </main>
     </div>
-  );
-};
+  )
+}
 
-export default ProfileSelectScreen;
+export default ProfileSelectScreen
