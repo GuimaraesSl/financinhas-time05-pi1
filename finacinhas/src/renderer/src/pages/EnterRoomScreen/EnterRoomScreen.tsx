c62b013413034ea61aafb774ffccd8746b211bdf
Gabriel Al-Samir <alsamir.gabriel@gmail.com>
2024-12-13 00:15:00 -0300

    fix: fixes entire enter room

--- finacinhas/src/renderer/src/pages/EnterRoomScreen/EnterRoomScreen.tsx
+++ finacinhas/src/renderer/src/pages/EnterRoomScreen/EnterRoomScreen.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import logo from '../assets/Logo-Subtitle.svg';
-import '../styles/EnterRoomScreen.css';
+import React from 'react'
+import logo from '../../assets/Logo-Subtitle.svg'
+import './EnterRoomScreen.css'
 
 const EnterRoomScreen: React.FC = () => {
   return (
     <div className="containerEnterRoomScreen">
       <header className="headerEnterRoomScreen">
-        <img src={logo} className="logoEnterRoomScreen" alt="logo"/>
+        <img src={logo} className="logoEnterRoomScreen" alt="logo" />
       </header>
       <main className="mainEnterRoomScreen">
         <h2>Digite o código para entrar na sala</h2>
-        <input type="text" className="inputEnterRoomScreen"/> 
+        <input type="text" className="inputEnterRoomScreen" />
         <button className="EnterRoomButton">CONTINUAR</button>
       </main>
     </div>
-  );
-};
+  )
+}
 
-export default EnterRoomScreen;
+export default EnterRoomScreen
